d4e27c275909fcd7c2b21ca5b70316473721f48c
Charles Thompson <135574264+thompsonc58@users.noreply.github.com>
2025-02-05 19:56:24 -0500

    uploading files

---
+++ VehicleClassLibrary/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleClassLibrary
+{
+    public class Car : Vehicle
+    {
+        public int SeatingCapacity {  get; set; }
+        public int numDoors { get; set; }
+        public Car(int capcity, int doors) { 
+        
+            SeatingCapacity = capcity;
+            numDoors = doors;
+        }
+
+        public Car() {
+            SeatingCapacity = 2;
+            numDoors = 2;
+        }
+        //methods do stuff
+
+        //
+        public override string ToString()
+        {
+            return Model.ToString() + " " + Brand.ToString()+ "  Price: "+ RentalPrice;
+        }
+    }
+}
---
+++ VehicleClassLibrary/RentalService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleClassLibrary
+{
+    public class RentalService
+    {
+        List<Vehicle> vehicles;
+        Car newCar = new Car();
+        Motorcycle newMoto = new Motorcycle();
+        
+        const decimal VehicleRentalCost = 100m;
+        const decimal CarRentalCost = 300m;
+        public RentalService() { }
+
+        //methods
+        public void RentCar()
+        {
+            //print out a list of cars for user to select
+            vehicles.Add(newCar);
+            vehicles.Add(newMoto);
+            foreach (var vehicle in vehicles) 
+            {
+                Console.WriteLine(string.Format("{0} - {1} - {2}", vehicle.Brand, vehicle.Model, vehicle.RentalPrice));
+            }
+            //Which car
+            Console.WriteLine("which car?");
+            string selection = Console.ReadLine();
+            Vehicle selected = vehicles.Find(x => x.Brand == selection);
+           
+            if (selected.isAvaliable)
+            {
+                selected.isAvaliable = false;
+                //perform rental; calculate price for days 
+            }
+            else
+            {
+                Console.WriteLine("car is not available");
+            }
+        }
+        public void AddCarToInventory()
+        {
+
+        }
+        public void RemoveCarFromInventory()
+        {
+
+        }
+
+    }
+}
